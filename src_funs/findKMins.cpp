fc3e8917f7df9db46bd04f0f5845890f8a18474a
csl <3079625093@qq.com>
2021-12-09 18:56:34 +0800

    change template params

--- src_funs/findKMins.cpp
+++ src_funs/findKMins.cpp
@@ -1,26 +1,25 @@
 #include <iostream>
 #include <vector>
-#include <functional>
 
 /**
  * @brief find the value that there are k values less than it in the value container
  * 
- * @tparam _Ty the type of the element
+ * @tparam _Container the type of the value container
  * @param vals the value container
- * @param k the parameter
- * @return _Ty the result
+ * @return _Container::value_type the type of return value
  */
-template <typename _Ty>
-_Ty findKMins(const std::vector<_Ty> &vals, std::size_t k)
+template <typename _Container>
+typename _Container::value_type findKMins(const _Container &vals, std::size_t k)
 {
+    using value_type = typename _Container::value_type;
     if (vals.size() == 0)
-        return _Ty();
+        return value_type();
     else if (vals.size() == 1)
         return vals.front();
     else
     {
-        _Ty a = vals.at(vals.size() / 2);
-        std::vector<_Ty> s1, s2, s3;
+        value_type a = vals.at(vals.size() / 2);
+        _Container s1, s2, s3;
         for (const auto &elem : vals)
             if (elem < a)
                 s1.push_back(elem);
@@ -37,15 +36,16 @@ _Ty findKMins(const std::vector<_Ty> &vals, std::size_t k)
     }
 }
 
+
 /**
  * @brief fing the media value in the value container
  * 
- * @tparam _Ty the type of the element
+ * @tparam _Container the type of the value container
  * @param vals the value container
- * @return _Ty the result
+ * @return _Container::value_type the type of return value
  */
-template <typename _Ty>
-_Ty findMedia(const std::vector<_Ty> &vals)
+template <typename _Container>
+typename _Container::value_type findMedia(const _Container &vals)
 {
     return findKMins(vals, vals.size() / 2);
 }
@@ -53,7 +53,7 @@ _Ty findMedia(const std::vector<_Ty> &vals)
 int main(int argc, char const *argv[])
 {
     std::vector<int> vals = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    
+
     std::cout << findKMins(vals, 3) << std::endl;
     std::cout << findKMins(vals, 4) << std::endl;
     std::cout << findMedia(vals) << std::endl;
