09096a77421d122160f33b5b17fb51719467e422
csl <3079625093@qq.com>
2022-07-01 11:16:07 +0800

    update julian.cpp

--- src/julian.cpp
+++ src/julian.cpp
@@ -4,12 +4,14 @@
 
 double julianDay(std::size_t year, std::size_t month, std::size_t day,
                  std::size_t hour = 0, std::size_t minute = 0, double second = 0.0) {
-  return 1721013.5 +
-         367.0 * year -
-         int(7.0 / 4.0 * (year + int((month + 9.0) / 12.0))) +
-         day +
-         (((second / 60.0) + minute) / 60.0 + hour) / 24.0 +
-         int(275.0 * month / 9.0);
+  if (month == 1 || month == 2) {
+    year -= 1;
+    month += 12;
+  }
+  int A = int(year / 100.0);
+  int B = 2 - A + int(A / 4.0);
+  return int(365.25 * (year + 4716)) + int(30.6001 * (month + 1)) + day +
+         ((second / 60.0 + minute) / 60.0 + hour) / 24.0 + B - 1524.5;
 }
 
 double modJulianDay(std::size_t year, std::size_t month, std::size_t day,
@@ -19,22 +21,39 @@ double modJulianDay(std::size_t year, std::size_t month, std::size_t day,
 
 std::tuple<std::size_t, std::size_t, std::size_t, std::size_t, std::size_t, double>
 fromJulianDay(double jd) {
-  int a = int(jd + 0.5);
-  int b = a + 1537;
-  int c = int((b - 122.1) / 365.25);
-  int d = int(365.25 * c);
-  int e = int((b - d) / 30.600);
-  double f = jd + 0.5;
-  double dms = b - d - int(30.6001 * e) + f - int(f);
-  std::size_t day = int(dms);
-  dms -= day, dms *= 24.0;
-  std::size_t hour = int(dms);
-  dms -= hour, dms *= 60.0;
-  std::size_t minute = int(dms);
-  dms -= minute, dms *= 60.0;
-  double second = dms;
-  std::size_t month = e - 1 - 12 * int(e / 24.0);
-  std::size_t year = c - 4715 - int((7 + month) / 10.0);
+  jd += 0.5;
+  int Z = int(jd);
+  double F = jd - Z;
+  int A;
+  if (Z < 2299161) {
+    A = Z;
+  } else {
+    int alpha = int((Z - 1867216.25) / 36524.25);
+    A = Z + 1 + alpha - int(alpha / 4.0);
+  }
+  int B = A + 1524;
+  int C = int((B - 122.1) / 365.25);
+  int D = int(365.25 * C);
+  int E = int((B - D) / 30.6001);
+  double dhs = B - D - int(30.6001 * E) + F;
+  std::size_t day = int(dhs);
+  dhs -= day, dhs *= 24.0;
+  std::size_t hour = int(dhs);
+  dhs -= hour, dhs *= 60.0;
+  std::size_t minute = int(dhs);
+  dhs -= minute, dhs *= 60.0;
+  double second = dhs;
+  std::size_t year, month;
+  if (E < 14) {
+    month = E - 1;
+  } else if (E == 14 || E == 15) {
+    month = E - 13;
+  }
+  if (month > 2) {
+    year = C - 4716;
+  } else if (month == 1 || month == 2) {
+    year = C - 4715;
+  }
   return {year, month, day, hour, minute, second};
 }
 
@@ -46,15 +65,17 @@ fromModJulianDay(double mjd) {
 int main(int argc, char const *argv[]) {
   std::cout << std::fixed << std::setprecision(3);
   {
-    std::cout << julianDay(2007, 10, 26, 9, 30) << std::endl;
-    auto [year, month, day, hour, minute, second] = fromJulianDay(2454399.896);
+    double jd = julianDay(2007, 10, 26, 9, 30);
+    std::cout << jd << std::endl;
+    auto [year, month, day, hour, minute, second] = fromJulianDay(jd);
     std::cout << year << '-' << month << '-' << day << ' '
               << hour << ':' << minute << ':' << second << std::endl;
   }
-  
+
   {
-    std::cout << modJulianDay(2007, 10, 26, 9, 30) << std::endl;
-    auto [year, month, day, hour, minute, second] = fromModJulianDay(54399.396);
+    double mjd = modJulianDay(2007, 10, 26, 9, 30);
+    std::cout << mjd << std::endl;
+    auto [year, month, day, hour, minute, second] = fromModJulianDay(mjd);
     std::cout << year << '-' << month << '-' << day << ' '
               << hour << ':' << minute << ':' << second << std::endl;
   }
