a1152fa63fdc4b7fff573a46d216555566096e28
csl <3079625093@qq.com>
2022-07-26 10:27:13 +0800

    add sort for func 'filesInDir'

--- src/listfiles.cpp
+++ src/listfiles.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <filesystem>
+#include <algorithm>
 
 /**
  * \brief a function to get all the filenames in the directory
@@ -13,12 +14,13 @@ std::vector<std::string> filesInDir(const std::string &directory)
     for (const auto &elem : std::filesystem::directory_iterator(directory))
         if (elem.status().type() != std::filesystem::file_type::directory)
             files.push_back(std::filesystem::canonical(elem.path()).c_str());
+    std::sort(files.begin(), files.end());
     return files;
 }
 
 int main(int argc, char const *argv[])
 {
-    auto fs = filesInDir("../src_funs");
+    auto fs = filesInDir("../src");
     for (const auto &filename : fs)
         std::cout << filename << std::endl;
     return 0;
